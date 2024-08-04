fb61baa688d1daaa6f774ae874bb4e4227539b4a
redmi <hmadaredmi7@gmail.com>
2024-08-04 18:45:04 +0300

    cahnge some style

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
 
   return (
     <div className='footer'>
-      <Link href={"/deleted"}><p>View all deleted tasks</p></Link>
+      <Link href={"/deleted"}><p className="text-red-600 hover:text-red-500 text-2xl">View all deleted tasks</p></Link>
     </div>
   )
 }
